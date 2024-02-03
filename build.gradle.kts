bc1990bccd9674c6ac210fa72a56ef5318bf7241
NGApps Dev <49076456+ngapp-dev@users.noreply.github.com>
2024-02-03 21:06:05 +0100

    update dependencies (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,5 +42,4 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
 }
